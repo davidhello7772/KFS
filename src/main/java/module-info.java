68cddc8bde4419c627ed413d1cf468016b040c58
David <david.dev.kbs@kadampa.net>
2024-06-08 14:58:23 +0100

    Change on pom.xml to solve the pb of the missing javafx application

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,7 @@ module org.kadampa.festivalstreaming {
     requires javafx.fxml;
     requires java.desktop;
     requires webcam.capture;
-    requires ffmpeg;
     requires com.google.gson;
-    requires org.fusesource.jansi;
 
     opens org.kadampa.festivalstreaming to com.google.gson;
     exports org.kadampa.festivalstreaming;
